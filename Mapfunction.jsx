934cbc35090e8059546a91eee3335317391775fd
Praveen Kannaujiya <praveenkannaujiya0@gmail.com>
2023-05-11 21:19:55 +0530

    React js code map code add in this code file

---
+++ Mapfunction.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Mapfunction = () => {
+    const user=[
+        {name:"Deny", class:"BCA", subject:"Computer Science" },
+        {name:"Akash", class:"BA", subject:"Art  Science" },
+        {name:"Abdullah", class:"D-Pharma", subject:" Bio Science" },
+    ]
+
+  return (
+    <>
+    <table>
+    <tr>
+                <td>Name</td>
+                <td>Class</td>
+                <td>Subject</td>
+  </tr>
+    {
+        user.map((item)=>{
+            <tr>
+                <td>{item.name}</td>
+                <td>{item.class}</td>
+                <td>{item.subject}</td>
+            </tr>
+        })
+    }
+</table>
+    </>
+  )
+}
+
+export default Mapfunction
